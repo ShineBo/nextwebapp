3a8a7889c755ae8845e56e383658d1d60ce0a184
Shine Bo Bo <shinebobo648@gmail.com>
2025-04-03 09:56:58 +0630

    updated some text

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const MyApp = () => {
   return (
-    <div>Hello World!</div>
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat quo debitis 
+        magni eius repudiandae veniam, eaque est, velit, nulla ducimus molestias? 
+        Laudantium sunt at itaque!
+      </p>
+    </div>
   )
 }
 
